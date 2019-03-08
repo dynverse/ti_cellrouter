3d624edf948a2152f1d8b5ada6170c39e43e5b24
Robrecht Cannoodt <rcannood@gmail.com>
2019-03-09 00:22:02 +0100

    updating cellrouter

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,11 @@
-FROM dynverse/dynwrap:bioc
+FROM dynverse/dynwrapr:v0.1.0
 
-RUN R -e 'devtools::install_cran(c("reshape", "reshape2", "pheatmap", "tsne", "igraph", "ggplot2", "mclust", "Rtsne", "cccd", "irlba"))'
+RUN R -e 'remotes::install_cran(c("reshape", "reshape2", "pheatmap", "tsne", "igraph", "ggplot2", "mclust", "Rtsne", "cccd", "irlba"))'
 
-RUN git clone https://github.com/edroaldo/cellrouter.git && find cellrouter -type f | grep -v "^cellrouter/CellRouter" | xargs rm
+RUN git clone --depth 1 https://github.com/edroaldo/cellrouter.git && find cellrouter -type f | grep -v "^cellrouter/CellRouter" | xargs rm
 
 RUN apt-get update && apt-get install -y default-jre
 
-LABEL version 0.1.8
+COPY run.R example.R definition.yml /code/
 
-ADD . /code
-ENTRYPOINT Rscript /code/run.R
+ENTRYPOINT /code/run.R
--- run.R
+++ run.R
@@ -1,62 +1,69 @@
-library(dplyr)
-library(purrr)
-library(readr)
-library(feather)
-library(tibble)
-library(igraph)
-source("/cellrouter/CellRouter_Class.R")
+#!/usr/local/bin/Rscript
 
+library(dyncli, warn.conflicts = FALSE)
 
 checkpoints <- list()
 
-#   ____________________________________________________________________________
-#   Load data                                                               ####
+#####################################
+###           LOAD DATA           ###
+#####################################
 
-data <- read_rds("/ti/input/data.rds")
-p <- jsonlite::read_json("/ti/input/params.json")
+task <- dyncli::main()
 
-if (length(p$seed) > 0 && is.finite(p$seed)) set.seed(p$seed)
+params <- task$params
+expression <- task$expression %>% as.matrix
 
-#' @examples
-#' data <- dyntoy::generate_dataset(model = "cyclic") %>% c(., .$prior_information)
-#' p <- yaml::read_yaml("containers/cellrouter/definition.yml")$parameters %>%
-#'   {.[names(.) != "forbidden"]} %>%
-#'   map(~ .$default)
+start_id <- task$priors$start_id
 
-counts <- data$counts
-start_id <- data$start_id
+# load software
+library(readr, warn.conflicts = FALSE)
+library(dplyr, warn.conflicts = FALSE)
+library(purrr, warn.conflicts = FALSE)
+library(dynwrap, warn.conflicts = FALSE)
+library(tibble, warn.conflicts = FALSE)
+# library(igraph)
 
-checkpoints$method_afterpreproc <- as.numeric(Sys.time())
+libdir <- "/cellrouter"
+# libdir <- "ti_cellrouter/cellrouter"
+source(paste0(libdir, "/CellRouter_Class.R"))
 
-#   ____________________________________________________________________________
-#   Infer trajectory                                                        ####
+# TIMING: done with preproc
+timings <- list(method_afterpreproc = Sys.time())
+
+#####################################
+###        INFER TRAJECTORY       ###
+#####################################
+
 # steps are taken from https://github.com/edroaldo/cellrouter/blob/master/Myeloid_Progenitors/CellRouter_Paul_Tutorial.md
-cellrouter <- CellRouter(as.data.frame(t(counts)))
-
-cellrouter <- Normalize(cellrouter)
+cellrouter <- CellRouter(as.data.frame(t(expression)))
 cellrouter <- scaleData(cellrouter)
 
 # do pca
-num_pcs <- pmin(p$ndim_pca, ncol(counts) - 1)
-cellrouter <- computePCA(cellrouter, num.pcs = num_pcs, seed = p$seed)
-
+num_pcs <- pmin(params$ndim_pca, ncol(expression) - 1)
+cellrouter <- computePCA(cellrouter, num.pcs = num_pcs, seed = NULL) # seed is set by dyncli
 
 # do safe tsne
-if (p$max_iter == "Inf") {p$max_iter <- 100000}
+if (p$max_iter == "Inf") {
+  params$max_iter <- 100000
+}
 
 again <- TRUE
-while(again) {
+while (again) {
   tryCatch({
-
-    cellrouter <- computeTSNE(cellrouter, num.pcs = p$ndim_tsne, seed = p$seed, max_iter = p$max_iter, perplexity = p$perplexity)
+    cellrouter <- computeTSNE(
+      cellrouter,
+      num.pcs = params$ndim_tsne,
+      max_iter = params$max_iter,
+      perplexity = params$perplexity,
+      seed = NULL
+    )
     again <- FALSE
   }, error = function(e) {
     if (grepl("Perplexity is too large", e$message)) {
       # don't forget both <<-'s, otherwise this will result in an infinite loop
       again <<- TRUE
-      cat("TSNE: Perplexity is too large. Reducing perplexity by 25%: ", p$perplexity, " -> ", p$perplexity * .75, "\n", sep = "")
-      p$perplexity <<- p$perplexity * .75
-
+      cat("TSNE: Perplexity is too large. Reducing perplexity by 25%: ", params$perplexity, " -> ", params$perplexity * .75, "\n", sep = "")
+      params$perplexity <<- params$perplexity * .75
     } else {
       stop(e)
     }
@@ -64,60 +71,81 @@ while(again) {
 }
 
 # louvain clustering
-cellrouter <- findClusters(cellrouter, method = "graph.clustering", num.pcs = min(p$ndim_pca_clustering, num_pcs), k = p$k_clustering)
+cellrouter <- findClusters(
+  cellrouter,
+  method = "graph.clustering",
+  num.pcs = min(params$ndim_pca_clustering, num_pcs),
+  k = params$k_clustering
+)
 
 # do knn
-cellrouter <- buildKNN(cellrouter, k = p$k_knn, column.ann = 'population', num.pcs = min(p$ndim_pca_knn, num_pcs), sim.type = p$sim_type)
+cellrouter <- buildKNN(
+  cellrouter,
+  k = params$k_knn,
+  column.ann = 'population',
+  num.pcs = min(params$ndim_pca_knn, num_pcs),
+  sim.type = params$sim_type
+)
 
 # create trajectory using start cells as source
-outputdir <- "/ti/workspace/"
-dir.create(outputdir, recursive = TRUE, showWarnings = FALSE)
+outputdir <- dynutils::safe_tempdir("cellrouter")
+on.exit(unlink(outputdir, recursive = TRUE))
 filename <- file.path(outputdir, "cell_edge_weighted_network.txt")
 write.table(cellrouter@graph$edges, file = filename, sep = '\t', row.names = FALSE, col.names = FALSE, quote = FALSE)
 
 sources <- unique(cellrouter@sampTab$population[cellrouter@sampTab$sample_id %in% start_id])
 targets <- setdiff(as.vector(cellrouter@sampTab$population), sources)
 
-libdir <- "/cellrouter/CellRouter/"
-cellrouter <- findPaths(cellrouter, column='population', libdir, outputdir, method = p$distance_method_paths) # this function uses global variables...
+cellrouter <- findPaths(
+  cellrouter,
+  column = 'population',
+  libdir,
+  outputdir,
+  method = params$distance_method_paths
+) # this function uses global variables...
 
-#Preprocess trajectories
+# process trajectories
 cellrouter <- processTrajectories(
   cellrouter,
   rownames(cellrouter@ndata),
-  path.rank = p$ranks,
-  num.cells = p$num_cells,
-  neighs = p$neighs,
+  path.rank = params$ranks,
+  num.cells = params$num_cells,
+  neighs = params$neighs,
   column.ann = 'population',
   column.color = 'colors'
 )
 
 checkpoints$method_aftermethod <- as.numeric(Sys.time())
 
-#   ____________________________________________________________________________
-#   Process cell graph                                                      ####
+#####################################
+###     SAVE OUTPUT TRAJECTORY    ###
+#####################################
 # first get network of backbone cells
-backbone_network <- map(cellrouter@paths$path, function(x) tail(head(stringr::str_split(x, "->")[[1]], -1), -1)) %>%
-  map(function(order) {
-    tibble(
-      from = order[-length(order)],
-      to = lead(order)[-length(order)],
-      directed = TRUE,
-      length = 1
-    )
-  }) %>%
-  bind_rows()
+backbone_network <-
+  map_df(
+    cellrouter@paths$path,
+    function(x) {
+      order <- tail(head(stringr::str_split(x, "->")[[1]], -1), -1)
+      tibble(
+        from = order[-length(order)],
+        to = lead(order)[-length(order)],
+        directed = TRUE,
+        length = 1
+      )
+    }
+  )
 
 # now get for every non-backbone cell the shortest backbone cell
 backbone_cells <- unique(c(backbone_network$from, backbone_network$to))
 nonbackbone_cells <- setdiff(rownames(counts), backbone_cells)
 
-nonbackbone_network <- distances(cellrouter@graph$network, backbone_cells, nonbackbone_cells) %>%
+nonbackbone_network <-
+  distances(cellrouter@graph$network, backbone_cells, nonbackbone_cells) %>%
   apply(2, which.min) %>%
   {backbone_cells[.]} %>%
   set_names(nonbackbone_cells) %>%
   enframe("from", "to") %>%
-  mutate(length=1, directed=TRUE)
+  mutate(length = 1, directed = TRUE)
 
 # combine to cell_graph & remove duplicated edges
 cell_graph <- bind_rows(
@@ -131,14 +159,25 @@ cell_graph <- bind_rows(
 to_keep <- backbone_cells
 
 # dimred
-dimred <- cellrouter@tsne %>% as.data.frame() %>% rownames_to_column("cell_id")
+dimred <-
+  cellrouter@tsne %>%
+  as.data.frame() %>%
+  rownames_to_column("cell_id")
 
 # save output
-output <- lst(
-  cell_ids = tibble(cell_ids = unique(c(cell_graph$from, cell_graph$to))),
-  cell_graph,
-  to_keep,
-  dimred,
-  timings = checkpoints
-)
-write_rds(output, "/ti/output/output.rds")
+output <-
+  wrap_data(
+    cell_ids = unique(c(cell_graph$from, cell_graph$to))
+  ) %>%
+  add_dimred(
+    dimred = dimred
+  )%>%
+  add_cell_graph(
+    cell_graph = cell_graph,
+    to_keep = to_keep
+  )  %>%
+  add_timings(
+    timings = checkpoints
+  )
+
+dyncli::write_output(output, task$output)
